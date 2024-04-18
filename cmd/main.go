f1afc13c8298615ccf64495fa0dbc165741d4ed0
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-04-18 13:12:17 +0000

    Implement the features

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/Squwid/go-randomizer"
+)
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Cool beans")
+	if len(os.Args) == 1 {
+		log.Fatalf("No arguments provided. Please provide either \"new\" or \"update\"!")
+	}
+
+	arg := os.Args[1]
+
+	switch arg {
+	case "new":
+		{
+			log.Printf("Creating a new branch!")
+			branchName := strings.Join(randomizer.Words(3), "-")
+			// Construct the git command
+			cmd := exec.Command("git", "checkout", "-b", branchName)
+
+			// Run the command
+			err := cmd.Run()
+			if err != nil {
+				log.Fatalf("Failed to execute command: %s", err)
+			}
+			log.Printf("New branch created: %s", branchName)
+		}
+	case "update":
+		{
+			log.Printf("Updating current branch!")
+			pullCmd := exec.Command("git", "pull")
+
+			err := pullCmd.Run()
+			if err != nil {
+				log.Fatalf("Failed to execute command: %s", err)
+			}
+			pullMainCmd := exec.Command("git", "pull", "origin", "main", "--no-rebase")
+
+			pullMainErr := pullMainCmd.Run()
+			if pullMainErr != nil {
+				log.Fatalf("Failed to execute command: %s", pullMainErr)
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hamlim/ggg
 
 go 1.22.2
+
+require github.com/Squwid/go-randomizer v0.1.2 // indirect
